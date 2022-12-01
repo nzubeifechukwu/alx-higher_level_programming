533a7fc4111154db735eefeea3bdbab885a3a86f
Nzube Ifechukwu <nzube.ifechukwu@gmail.com>
2022-12-01 06:04:47 -0800

    Add 3-infinite_add.py

---
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+if __name__ == '__main__':
+    from sys import argv
+    sum_av = 0
+    for i in range(1, len(argv)):
+        sum_av = sum_av + int(argv[i])
+    print(sum_av)
