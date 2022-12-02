6819f52e1443049c964b4031b46faf3b1eaf5f51
Nzube Ifechukwu <nzube.ifechukwu@gmail.com>
2022-12-02 06:52:01 -0800

    Add 0-print_list_integer.py

---
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def print_list_integer(my_list=[]):
+    '''prints integers in a list
+
+    Arg:
+        my_list: a list of integers
+    '''
+    for n in my_list:
+        print('{}'.format(n))
