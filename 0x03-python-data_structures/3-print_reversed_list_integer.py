5b00ae5b0021d1892121e66bdb0ce29e6729806a
Nzube Ifechukwu <nzube.ifechukwu@gmail.com>
2022-12-02 07:23:26 -0800

    Add 3-print_reversed_list_integer.py

---
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def print_reversed_list_integer(my_list=[]):
+    '''Prints all integers of a list in reverse
+
+    Arg:
+        my_list: list
+    '''
+    my_list.reverse()
+    for n in my_list:
+        print('{}'.format(n))
