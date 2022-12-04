cf2ba9fd4de198ae6759f2ebcb3270321e56b066
Nzube Ifechukwu <nzube.ifechukwu@gmail.com>
2022-12-03 23:23:59 -0800

    Add 6-print_matrix_integer.py

---
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+def print_matrix_integer(matrix=[[]]):
+    '''prints a matrix of integers
+
+    Args:
+        matrix: 2-d list of lists
+    '''
+    # mat_elements = [row[i] for row in matrix for i in len(row)]
+    mat_elems = [n for row in matrix for n in row]
+    for row in matrix:
+        for i in range(len(row)):
+            if i < len(row) - 1:
+                print('{}'.format(row[i]), end=' ')
+            else:
+                print('{}'.format(row[i]), end='')
+        print()
