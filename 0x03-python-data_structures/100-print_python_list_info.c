0f3f5ce5cbe82d559b5fba66cc813081b44c247a
Nzube Ifechukwu <nzube.ifechukwu@gmail.com>
2022-12-06 14:04:52 +0000

    Update 100-print_python_list_info.c

--- 0x03-python-data_structures/100-print_python_list_info.c
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -9,6 +9,7 @@ void print_python_list_info(PyObject *p)
 {
 	Py_ssize_t i;
 	PyListObject *l;
+	//struct _typeobject *item_type = p->ob_type
 
 	l = (PyListObject *)p;
 
@@ -17,6 +18,6 @@ void print_python_list_info(PyObject *p)
 
 	for (i = 0; i < PyList_Size(p); i++)
 	{
-		printf("Element %ld: \n", i/*, typeof(p[i])*/);
+		printf("Element %ld: %s\n", i, l->ob_item[i]->ob_type->tp_name);
 	}
 }
