87c1a6b9148c99d15c4a014b4b76f5d8f0aaf985
Nzube Ifechukwu <nzube.ifechukwu@gmail.com>
2022-12-07 05:35:25 -0800

    Add 3-common_elements.py

---
+++ 0x04-python-more_data_structures/3-common_elements.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def common_elements(set_1, set_2):
+    '''Returns a set of common elements in two sets
+
+    Args:
+        set_1: set
+        set_2: set
+
+    Returns:
+        Set of common elements in the two sets
+    '''
+    return set_1 & set_2
