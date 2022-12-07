35b1033c7e605d03042792bc7c667f1b01afc9e1
Nzube Ifechukwu <nzube.ifechukwu@gmail.com>
2022-12-07 05:29:41 -0800

    Add 2-uniq_add.py

---
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+def uniq_add(my_list=[]):
+    '''Adds all unique integers in a list
+
+    Arg:
+        my_list: list of integers
+
+    Return:
+        Sum of the unique integers
+    '''
+    uniq_sum = 0
+    for n in set(my_list):
+        uniq_sum += n
+    return uniq_sum
