f8b38c71cea04f461c97654f3a18e93ee6c4dba7
Nzube Ifechukwu <nzube.ifechukwu@gmail.com>
2022-12-07 05:46:51 -0800

    Add 5-number_keys.py

---
+++ 0x04-python-more_data_structures/5-number_keys.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def number_keys(a_dictionary):
+    '''Computes the number of keys in a dictionary
+
+    Arg:
+        a_dictionary: dictionary
+
+    Returns:
+        number of keys in a dictionary
+    '''
+    return len(a_dictionary)
