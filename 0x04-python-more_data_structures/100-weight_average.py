19e9832decc3c76ac77a3c7b5e63ea643e947ebb
Nzube Ifechukwu <nzube.ifechukwu@gmail.com>
2022-12-10 00:32:49 -0800

    Add 100-weight_average.py

---
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+def weight_average(my_list=[]):
+    '''Computes weighted average of list of tuples of the form (score, weight)
+
+    Arg:
+        my_list: list of tuples
+
+    Return:
+        weighted average or 0 if list is empty
+    '''
+    num = 0
+    den = 0
+    for tup in my_list:
+        num = num + (tup[0] * tup[1])
+        den = den + tup[1]
+    return (num / den)
