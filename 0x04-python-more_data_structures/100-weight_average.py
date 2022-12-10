fed7633dd1926b3173e22838c1c73f937e2cbf35
Nzube Ifechukwu <nzube.ifechukwu@gmail.com>
2022-12-10 00:36:09 -0800

    Update 100-weight_average.py

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -10,7 +10,12 @@ def weight_average(my_list=[]):
     '''
     num = 0
     den = 0
+
+    if my_list == []:
+        return 0
+
     for tup in my_list:
         num = num + (tup[0] * tup[1])
         den = den + tup[1]
+
     return (num / den)
