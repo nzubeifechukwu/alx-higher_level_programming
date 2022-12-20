b54e6396d301177ab667217dd55a61545272ab7e
Nzube Ifechukwu <nzube.ifechukwu@gmail.com>
2022-12-20 01:19:46 -0800

    Add 1-safe_print_integer.py

---
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+def safe_print_integer(value):
+    '''Prints an integer
+
+    Arg:
+        value: integer to print
+
+    Return:
+        True if value successfully printed, False otherwise
+    '''
+    try:
+        print('{:d}'.format(value))
+    except ValueError:
+        return False
+    return True
