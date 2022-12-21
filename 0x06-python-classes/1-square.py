a11469579c7478bf7e96cf4d429272c0912188d6
Nzube Ifechukwu <nzube.ifechukwu@gmail.com>
2022-12-21 05:34:39 -0800

    Add 1-square.py

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+class Square:
+    '''A Square of size size
+
+    '''
+    def __init__(self, size):
+        '''The __init__ method defines the size of the square
+
+        Args:
+            size: Size of the square
+
+        '''
+        self.__size = size
