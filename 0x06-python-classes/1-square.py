106bd4366add494f48b0a1829e3fa7e365e9df89
Nzube Ifechukwu <nzube.ifechukwu@gmail.com>
2022-12-21 05:51:51 -0800

    Update 1-square.py

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,10 +1,12 @@
 #!/usr/bin/python3
-class Square:
-    '''A Square of size size
+'''Defines a square'''
 
-    '''
+
+class Square:
+    '''Represents a square'''
+
     def __init__(self, size):
-        '''The __init__ method defines the size of the square
+        '''Initializes a square
 
         Args:
             size: Size of the square
