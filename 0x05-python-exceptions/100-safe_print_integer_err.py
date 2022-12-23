d332b3223aecd8cc5baa81422862353a49938321
Nzube Ifechukwu <nzube.ifechukwu@gmail.com>
2022-12-23 00:59:33 -0800

    Add 100-safe_print_integer_err.py

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -11,6 +11,6 @@ def safe_print_integer_err(value):
     try:
         print('{:d}'.format(value))
         return True
-    except ValueError:
+    except (ValueError, TypeError):
         print('Exception: {}'.format(sys.exc_info()[1]), file=sys.stderr)
         return False
