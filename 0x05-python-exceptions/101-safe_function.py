9b425df131a4c199e3bfc745256848827e3a55cf
Nzube Ifechukwu <nzube.ifechukwu@gmail.com>
2022-12-23 01:19:52 -0800

    Add 101-safe_function.py

---
+++ 0x05-python-exceptions/101-safe_function.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+import sys
+
+
+def safe_function(fct, *args):
+    '''Executes a function safely
+
+    Args:
+        fct: Points to the function
+        *args: One or more arguments
+
+    Returns:
+        The functions return value on successful execution
+        None on failure
+    '''
+    try:
+        return fct(*args)
+    except Exception as e:
+        print('Exception: {}'.format(e), file=sys.stderr)
+        return None
