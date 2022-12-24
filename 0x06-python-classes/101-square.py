3024b71c261ce13a21965058f20b0db2684fa762
Nzube Ifechukwu <nzube.ifechukwu@gmail.com>
2022-12-24 07:27:04 -0800

    Add 101-square.py

---
+++ 0x06-python-classes/101-square.py
@@ -0,0 +1,79 @@
+#!/usr/bin/python3
+'''Defines a square'''
+
+
+class Square:
+    '''Represents a square'''
+
+    def __init__(self, size=0, position=(0, 0)):
+        '''Initializes the square
+        Args:
+            size: Size of the square. Defaults to 0
+            position: Coordinates of the square. Defaults to (0, 0)
+        '''
+        self.size = size
+        self.position = position
+
+    def __str__(self):
+        '''Prints the square'''
+        return self.my_print()[:-1]
+
+    @property
+    def size(self):
+        '''Gets the size
+        Raises:
+            TypeError: If size is not an integer
+            ValueError: if size is less than 0
+        '''
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if type(value) is not int:
+            raise TypeError('size must be an integer')
+        elif value < 0:
+            raise ValueError('size must be >= 0')
+        else:
+            self.__size = value
+
+    @property
+    def position(self):
+        '''Gets the position of the square
+        Raises:
+            TypeError: If position is not a tuple of two positive integers
+        '''
+        return self.__position
+
+    @position.setter
+    def position(self, value):
+        message = 'position must be a tuple of 2 positive integers'
+        if len(value) != 2:
+            raise TypeError(message)
+        for n in value:
+            if type(n) is not int:
+                raise TypeError(message)
+            if n < 0:
+                raise TypeError(message)
+        self.__position = value
+
+    def area(self):
+        '''Computes the area of the square
+        Returns:
+            Area of the square
+        '''
+        return (self.size ** 2)
+
+    def my_print(self):
+        '''Prints the square at the correct position using #'''
+        sq = ''
+        if self.size == 0:
+            sq = '\n'
+        else:
+            for i in range(self.position[1]):
+                sq = sq + '\n'
+            for i in range(self.size):
+                sq = sq + (' ' * self.position[0])
+                for j in range(self.size):
+                    sq = sq + '#'
+                sq = sq + '\n'
+        return sq
