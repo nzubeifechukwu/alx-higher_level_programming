e778e64387461079f51521fee6523f73b3b69158
Nzube Ifechukwu <nzube.ifechukwu@gmail.com>
2023-01-09 06:16:41 -0800

    Add 6-base_geometry.py

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+'''Defines a BaseGeometry class
+'''
+
+
+class BaseGeometry:
+    '''Represents a BaseGeometry class
+    '''
+
+    def area(self):
+        '''Computes the area of the basegeometry instance
+        '''
+        raise Exception('area() is not implemented')
