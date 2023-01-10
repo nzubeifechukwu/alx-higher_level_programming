495ff104803ce0a26746e59c616bf697edb29a86
Nzube Ifechukwu <nzube.ifechukwu@gmail.com>
2023-01-10 08:30:16 -0800

    Update 6-load_from_json_file.py

--- 0x0B-python-input_output/6-load_from_json_file.py
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -11,4 +11,4 @@ def load_from_json_file(filename):
         filename: name of JSON file
     '''
     with open(filename, 'r', encoding='utf-8') as f:
-        json.load(f)
+        return json.load(f)
