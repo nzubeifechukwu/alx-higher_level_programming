38609745c333070d708278bef7903ca7cbc267ca
Nzube Ifechukwu <nzube.ifechukwu@gmail.com>
2023-01-10 11:36:00 -0800

    Update 5-save_to_json_file.py

--- 0x0B-python-input_output/5-save_to_json_file.py
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -11,5 +11,5 @@ def save_to_json_file(my_obj, filename):
         my_obj: object to write
         filename: text file to write to
     '''
-    with open(filename, 'w', encoding='utf-8') as f:
+    with open(filename, 'a', encoding='utf-8') as f:
         json.dump(my_obj, f)
