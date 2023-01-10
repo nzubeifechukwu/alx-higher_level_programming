3cba5eb31aad86bcb9af900a48948deab9d0128b
Nzube Ifechukwu <nzube.ifechukwu@gmail.com>
2023-01-10 06:32:47 -0800

    Add 2-append_write.py

---
+++ 0x0B-python-input_output/2-append_write.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+'''The following function appends a string at the end of a text file (UTF-8)
+    and returns the number of characters appended
+'''
+
+
+def append_write(filename='', text=''):
+    '''Appends a string at the end of a text file
+
+    Args:
+        filename: name of the file to append to
+        text: string to append
+
+    Returns:
+       number of characters appended 
+    '''
+    with open(filename, 'a', encoding='utf-8') as f:
+        return f.write(text)
