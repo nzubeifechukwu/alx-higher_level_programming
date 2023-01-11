d5e0d50aca038c0664051b93faa695f6abb34204
Nzube Ifechukwu <nzube.ifechukwu@gmail.com>
2023-01-11 04:14:47 -0800

    Add 10-student.py

---
+++ 0x0B-python-input_output/10-student.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+'''Defines a Student class
+'''
+
+
+class Student:
+    '''Represents a Student class
+    '''
+
+    def __init__(self, first_name, last_name, age):
+        '''Instantiates a Student object
+
+        Args:
+            first_name: first name of a Student instance
+            last_name: last name of a Student instance
+            age: age of a Student instance
+        '''
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        '''Returns the dict representation of a Student instance
+        '''
+        dict_rep = {}
+        ser = [list, dict, str, int, bool]
+        if type(attrs) is not list:
+            for a in dir(self):
+                if not a.startswith('__') and type(getattr(self, a)) in ser:
+                    dict_rep[a] = getattr(self, a)
+        else:
+            if all(type(a) is str for a in attrs):
+                for a in attrs:
+                    if not a.startswith('__') and a in dir(self):
+                        dict_rep[a] = getattr(self, a)
+        return dict_rep
