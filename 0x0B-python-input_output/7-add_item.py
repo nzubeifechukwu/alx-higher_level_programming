c25b2da7e793e679a64b7282f6dccfd737e84c7a
Nzube Ifechukwu <nzube.ifechukwu@gmail.com>
2023-01-10 19:02:39 -0800

    Add 7-add_item.py

---
+++ 0x0B-python-input_output/7-add_item.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+'''This module adds all arguments to a Python list and saves them to a file
+'''
+import sys
+
+
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+
+filename = 'add_item.json'
+
+# create a JSON representation of an empty list and save it in filename
+save_to_json_file([], filename)
+
+# Since filename now holds the JSON representation of an empty list,
+# arguments will hold an empty list after filename is decoded
+arguments = load_from_json_file(filename)
+
+# for each time this module is run with command line arguments,
+# append the arguments to the existing arguments list.
+# Then create new JSON representation of the updated arguments list
+# and save it in filename
+for arg in sys.argv[1:]:
+    arguments.append(arg)
+    save_to_json_file(arguments, filename)
