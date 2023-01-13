cde6800dd3a28d5b8a13b81156dc70810d0f7ea0
Nzube Ifechukwu <nzube.ifechukwu@gmail.com>
2023-01-12 23:59:56 -0800

    Add models/rectangle.py

---
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -0,0 +1,93 @@
+#!/usr/bin/python3
+'''Defines a Rectangle class that inherits from the Base class
+'''
+from models.base import Base
+
+
+class Rectangle(Base):
+    '''Represents a Rectangle class that inherits from the Base class
+
+    Args:
+        width: width of the Rectangle instance
+        height: height of the Rectangle instance
+        x: horizontal position of the Rectangle instance
+        y: vertical position of the Rectangle instance
+        id: id of the Rectangle instance
+    '''
+    def __init__(self, width, height, x=0, y=0, id=None):
+        super().__init__(id)
+        self.width = width
+        self.height = height
+        self.x = x
+        self.y = y
+
+    @property
+    def width(self):
+        '''Gets and sets the width attributes
+
+        Raises:
+            TypeError: if value is not int
+            ValueError: if value less than or equal to 0
+        '''
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        if type(value) is not int:
+            raise TypeError('width must be an integer')
+        if value <= 0:
+            raise ValueError('width must be > 0')
+        self.__width = value
+
+    @property
+    def height(self):
+        '''Gets and sets the height attribute
+
+        Raises:
+            TypeError: if value is not int
+            ValueError: if value is less than or equal to 0
+        '''
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        if type(value) is not int:
+            raise TypeError('height must be an integer')
+        if value <= 0:
+            raise ValueError('height must be > 0')
+        self.__height = value
+
+    @property
+    def x(self):
+        '''Gets and sets the attribute x
+
+        Raises:
+            TypeError: if value is not int
+            ValueError: if value is less than 0
+        '''
+        return self.__x
+
+    @x.setter
+    def x(self, value):
+        if type(value) is not int:
+            raise TypeError('x must be an integer')
+        if value < 0:
+            raise ValueError('x must be >= 0')
+        self.__x = value
+
+    @property
+    def y(self):
+        '''Gets and sets the y attribute
+
+        Raises:
+            TypeError: if y is not int
+            ValueError: if y is less than 0
+        '''
+        return self.__y
+
+    @y.setter
+    def y(self, value):
+        if type(value) is not int:
+            raise TypeError('y must be an integer')
+        if value < 0:
+            raise ValueError('y must be >= 0')
