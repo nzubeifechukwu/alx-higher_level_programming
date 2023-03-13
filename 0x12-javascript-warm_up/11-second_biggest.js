4f0f467eefd8fc2513a45140939cadbf5a354c81
Nzube Ifechukwu <nzube.ifechukwu@gmail.com>
2023-03-13 13:18:57 -0700

    Update 11-second_biggest.js

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -4,11 +4,19 @@
  * Finds the second biggest integer in a list of arguments
  */
 const len = process.argv.length;
-const args = process.argv;
+let args = process.argv;
 
 if (len <= 3) {
   console.log(0);
 } else {
-  args.sort();
-  console.log(parseInt(args[len - 2]));
+  args = args.slice(2);
+  const newArgs = [];
+  for (const n of args) {
+    newArgs.push(parseInt(n));
+  }
+  newArgs.sort(function (a, b) { return a - b; }); // sort int arr in asc order
+  console.log(newArgs);
+  const distinct = [...new Set(newArgs)]; // keeps only distinct values
+  console.log(distinct);
+  console.log(distinct[distinct.length - 2]);
 }
