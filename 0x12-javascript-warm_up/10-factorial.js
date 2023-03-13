7bb7bc612dd9c890b089dc058845a63c4f3c4e7e
Nzube Ifechukwu <nzube.ifechukwu@gmail.com>
2023-03-13 10:10:27 -0700

    Add 10-factorial.js

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -3,11 +3,11 @@
 /*
  * Computes a factorial
  */
-const factorial = (num) => {
+function factorial (num) {
   if (num === 0 || String(num) === 'NaN') {
     return 1;
   }
   return num * factorial(num - 1);
-};
+}
 
 console.log(factorial(parseInt(process.argv[2])));
