d6a7d9fda74ae65442acdbb11124f8813454f469
Nzube Ifechukwu <nzube.ifechukwu@gmail.com>
2023-03-13 08:06:06 -0700

    Update 6-multi_languages_loop.js

---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+
+/*
+ * Prints an array of string using a loop
+ */
+const strArr = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+for (const str of strArr) {
+  console.log(str);
+}
