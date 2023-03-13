7423cca9447bdecee09dc47f5caa1ffb135e3e77
Nzube Ifechukwu <nzube.ifechukwu@gmail.com>
2023-03-13 07:50:08 -0700

    Add 5-to_integer.js

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -6,5 +6,5 @@
 if (isNaN(process.argv[2])) {
   console.log('Not a number');
 } else {
-  console.log('My Number: ' + parseInt(process.argv[2]));
+  console.log('My number: ' + parseInt(process.argv[2]));
 }
