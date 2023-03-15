33023d18151e3fab94539250be97a32b5eae7c19
Nzube Ifechukwu <nzube.ifechukwu@gmail.com>
2023-03-15 08:52:28 -0700

    Update 100-map.js

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -4,6 +4,7 @@
  * Import an array and compute a new array
  */
 const list = require('./100-data').list;
-const newList = list.map(x => x * list.indexOf(x));
+let i = 0;
+const newList = list.map(function (x) { x = x * i; i++; return x; });
 console.log(list);
 console.log(newList);
