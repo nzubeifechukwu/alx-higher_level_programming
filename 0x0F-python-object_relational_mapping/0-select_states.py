ed3c8f4bd57cdc1a29adacdbd9416c21642feb1e
Nzube Ifechukwu <nzube.ifechukwu@gmail.com>
2023-03-16 08:00:35 -0700

    Update 0-select_states.py 1-filter_states.py

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,14 +1,18 @@
 #!/usr/bin/python3
-# Lists all states from a country database
+'''Lists all states from a country database'''
+
 
 import MySQLdb
+from sys import argv
 
-conn = MySQLdb.connect(host="localhost", port=3306, user="root", passwd="root", db="hbtn_0e_0_usa", charset="utf8")
-cur = conn.cursor()
-cur.execute("SELECT * FROM states ORDER BY id")
-rows = cur.fetchall()
-for row in rows:
-    print(row)
-cur.close()
-conn.close()
 
+if __name__ == '__main__':
+    conn = MySQLdb.connect(
+        host="localhost", port=3306, user=argv[1], passwd=argv[2], db=argv[3])
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM states ORDER BY id")
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
+    cur.close()
+    conn.close()
--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,12 +1,17 @@
 #!/usr/bin/python3
-# Lists all states with name starting with `N`
+'''Lists all states with name starting with `N`'''
+
 
 import MySQLdb
-conn = MySQLdb.connect(host="localhost", port=3306, user="root", passwd="root", db="hbtn_0e_0_usa", charset="utf8")
-cur = conn.cursor()
-cur.execute('SELECT * FROM states WHERE name LIKE "N%"')
-rows = cur.fetchall()
-for row in rows:
-    print(row)
-cur.close()
-conn.close()
+from sys import argv
+
+if __name__ == '__main__':
+    conn = MySQLdb.connect(
+        host="localhost", port=3306, user=argv[1], passwd=argv[2], db=argv[3])
+    cur = conn.cursor()
+    cur.execute('SELECT * FROM states WHERE name LIKE "N%"')
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
+    cur.close()
+    conn.close()
