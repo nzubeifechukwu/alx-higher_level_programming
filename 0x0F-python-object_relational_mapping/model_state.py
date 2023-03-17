4acf088fbb17488a3e09f6bc315649f2249580cc
Nzube Ifechukwu <nzube.ifechukwu@gmail.com>
2023-03-17 03:17:46 -0700

    Update model_state.py

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -9,7 +9,6 @@ represents a column of a string with maximum 128 characters and can’t be null
 
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import create_engine, Column, Integer, String, Sequence
-# from sys import argv
 
 Base = declarative_base()
 
@@ -21,7 +20,3 @@ class State(Base):
     seq = Sequence('state_id_seq')
     id = Column(Integer, seq, primary_key=True, nullable=False)
     name = Column(String(128), nullable=False)
-
-    # engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}\
-    #    '.format(argv[1], argv[2], argv[3]), pool_pre_ping=True)
-    # Base.metadata.create_all(engine)
