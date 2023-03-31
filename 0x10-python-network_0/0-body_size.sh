bafe2044887252d4054cc26f5b661b53b8e7ddb4
Nzube Ifechukwu <nzube.ifechukwu@gmail.com>
2023-03-31 02:12:52 -0700

    Update 0-body_size.sh

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,3 +1,3 @@
-#!/usr/bin/bash
+#!/bin/bash
 # This script accepts a url, sends a request to the url and displays the size of the body response in bytes
-curl -si "$1" | grep "Content-Length" | cut -d " " -f 2
+curl -sI "$1" | grep "Content-Length" | cut -d " " -f 2
