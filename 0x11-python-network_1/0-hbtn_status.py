da55726765f04cc6fc4bc132efe5446604003f7e
Nzube Ifechukwu <nzube.ifechukwu@gmail.com>
2023-03-31 14:07:50 -0700

    Add 0-hbtn_status.py

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+'''Fetch a url using the urllib package
+'''
+import urllib.request
+
+if __name__ == '__main__':
+    url = 'https://alx-intranet.hbtn.io/status'
+    with urllib.request.urlopen(url) as response:
+        html = response.read()
+    print('Body response:')
+    print('\t- type:', type(html))
+    print('\t- content:', html)
+    print('\t- utf8 content:', html.decode())
