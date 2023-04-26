bd1879a77e4272899068ff1884b89121bbb808bc
Nzube Ifechukwu <nzube.ifechukwu@gmail.com>
2023-04-25 19:15:49 -0700

    Add README.md and 0-readme.js

---
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -0,0 +1,15 @@
+#!/usr/bin/node
+/**
+ * Read, in utf-8, and print the content of a file,
+ * where the first argument is the file path.
+ * If an error occurred while reading, print the error object.
+ */
+const fs = require('fs');
+const process = require('process');
+
+fs.readFile(process.argv[2], function (error, data) {
+	if (error) {
+		return console.error(error);
+	}
+	console.log(data.toString());
+});
